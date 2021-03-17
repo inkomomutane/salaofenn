fda757339d41799dea781c1a8b9d14e602ddea3e
nelson_mutane <57417802+inkomomutane@users.noreply.github.com>
2021-03-17 20:26:11 +0200

    last commit

--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="container py-2">
     <div class="row justify-content-center">
         <div class="col-md-12 ">
-             <category-crud></category-crud>  
+             
         </div>
     </div>
 </div>
