b37d9d451f958b7ca38f037b1af8736033b293ac
nelson_mutane <57417802+inkomomutane@users.noreply.github.com>
2021-03-17 19:24:12 +0200

    setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -349,19 +349,15 @@
         </nav>
 
         <!-- Start main left sidebar menu -->
-        <div class="main-sidebar sidebar-style-2">
+        <div class="main-sidebar sidebar-style-2" style="background:none !important;border:none !important; box-shadow:none !important">
             <aside id="sidebar-wrapper">
-                <div class="sidebar-brand">
+                <div class="sidebar-brand shadow-sm bg-white">
                     <a href="{{ url('/') }}">{{ config('app.name','Fenn\'s Look') }}</a>
                 </div>
-                <div class="sidebar-brand sidebar-brand-sm">
+                <div class="sidebar-brand sidebar-brand-sm shadow-sm bg-white ">
                     <a href="{{ url('/') }}">F'L</a>
                 </div>
-                <ul class="sidebar-menu">
-                    <li class="menu-header">Dashboard</li>
-
-                    <li class=active><a class="nav-link" href="{{ route('home') }}"><i class="far fas fa-th-large"></i> <span>Dashboard</span></a></li>
-                </ul>
+              
             </aside>
         </div>
 
